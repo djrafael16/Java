4baeccff8521e7663abce7d7c0483952c2097c6c
djrafael16 <djrafael16@gmail.com>
2023-11-27 21:25:22 -0300

    Teste

---
+++ VaiNaWebT3/src/br/com/vainaweb/backendt3/Desafio/Livros.java
@@ -0,0 +1,21 @@
+package br.com.vainaweb.backendt3.Desafio;
+
+public class Livros {
+
+	String nome; 
+	String genero;
+	String autor;
+	int paginas;
+	
+	public Livros (String nome, String genero, String autor, int paginas)
+	{
+		this.nome = nome; 
+		this.genero = genero; 
+		this.autor = autor;
+		this.paginas = paginas; 
+		
+		
+	}
+
+	
+}
