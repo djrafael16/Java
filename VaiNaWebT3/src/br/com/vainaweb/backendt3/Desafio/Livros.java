379ff93f4054d979b92d46fe6fafcb6afbc4b526
djrafael16 <djrafael16@gmail.com>
2023-11-28 18:54:50 -0300

    Atualização 28/11

--- VaiNaWebT3/src/br/com/vainaweb/backendt3/Desafio/Livros.java
+++ VaiNaWebT3/src/br/com/vainaweb/backendt3/Desafio/Livros.java
@@ -1,24 +1,24 @@
 package br.com.vainaweb.backendt3.Desafio;
 
 public class Livros {
-
+  
 	String nome; 
 	String genero;
 	String autor;
 	int paginas;
 	int ano;
+	int id;
 	
-	public Livros (String nome, String genero, String autor, int paginas, int ano)
+	
+	public Livros (String nome, String genero, String autor, int paginas, int ano, int id)
 	{
 		this.nome = nome; 
 		this.genero = genero; 
 		this.autor = autor;
 		this.paginas = paginas; 
 		this.ano = ano; 
-		
+		this.id = id;
 		
 		
 	}
-
-	
 }
