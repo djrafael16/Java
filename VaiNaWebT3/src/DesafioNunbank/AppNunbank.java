3e54fee1304e42c970a1698dd75b4e0018aa715f
djrafael16 <djrafael16@gmail.com>
2024-02-26 21:35:21 -0300

    Atualização criando menu

--- VaiNaWebT3/src/DesafioNunbank/AppNunbank.java
+++ VaiNaWebT3/src/DesafioNunbank/AppNunbank.java
@@ -3,7 +3,7 @@ package DesafioNunbank;
 public class AppNunbank {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- VaiNaWebT3/src/DesafioNunbank/Conta.java
+++ VaiNaWebT3/src/DesafioNunbank/Conta.java
@@ -1,5 +1,84 @@
 package DesafioNunbank;
 
 public class Conta {
+	
+	private String nome;
+	private String cpf;
+	private int numeroconta;
+	private byte agencia;
+	private int senha;
+	private double saldo;
+	
+	
+	public Conta ()
+	{
+		
+	}
+	
+	public  Conta(String nome, String cpf, int numeroconta, byte agencia, int senha, double saldo) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.numeroconta = numeroconta;
+		this.agencia = agencia;
+		this.senha = senha;
+		this.saldo = saldo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getNumeroconta() {
+		return numeroconta;
+	}
+
+	public void setNumeroconta(int numeroconta) {
+		this.numeroconta = numeroconta;
+	}
+
+	public byte getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(byte agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getSenha() {
+		return senha;
+	}
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	@Override
+	public String toString() {
+		return "Conta [nome=" + nome + ", cpf=" + cpf + ", numeroconta=" + numeroconta + ", agencia=" + agencia
+				+ ", senha=" + senha + ", saldo=" + saldo + "]";
+	}
+	
+	
 
 }
